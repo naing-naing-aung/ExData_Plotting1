67ac3e25cc8a629bc8a42df73b452a297123870a
Naing Naing Aung <naing2aung@gmail.com>
2020-06-17 19:41:20 +0630

    Draw plot2

---
+++ plot2.R
@@ -0,0 +1,23 @@
+library(tidyverse)
+link <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+download.file(link, "house.zip", method = "curl")
+house <- read.csv2(unzip("house.zip"), na.strings = "?")
+house1 <- house %>%
+        mutate(Date = dmy(Date), Time = hms(Time)) %>%
+        filter(Date=="2007-02-01" | Date=="2007-02-02") %>%
+        mutate_if(is.character, as.numeric)
+h1 <- house1 %>%
+        filter(Date=="2007-02-01")
+h2 <- house1 %>%
+        filter(Date=="2007-02-02") %>%
+        mutate(Time = Time + hours(24))
+dat <- bind_rows(h1, h2)
+ggplot(data = dat, mapping = aes(as.numeric(Time), Global_active_power)) +
+        geom_path() +
+        scale_x_time(breaks = c("0", "86400", "172800"),
+                     labels = c("Thu", "Fri", "Sat")) +
+        labs(x = "", y = "Global Active Power (kilowatts)")
+
+
+dev.copy(png, "plot2.png")
+dev.off()
